4722e0464eabe150d8d573f3e7b73981ac08c2fe
nreHieW <limweihern@gmail.com>
2023-07-01 17:49:19 +0800

    Remove Testing action

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-import os
-
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-if not os.path.exists('data'):
-        os.makedirs('data')
-df.to_csv('data/test.csv', index=False)
