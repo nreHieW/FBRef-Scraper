5916ce8a1aaf3f8e29a78270d776e7b18999d161
nreHieW <limweihern@gmail.com>
2023-07-01 22:33:26 +0800

    Test Gcloud

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import pandas as pd 
-import os
+import os 
+project_id = os.environ["GCP_PROJECT_NAME"]
 
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-if not os.path.exists('data'):
-        os.makedirs('data')
-df.to_csv('data/test.csv', index=False)
+cols = ["Standard_Player", 'Playing_Time_Playing_Time_Min',"Standard_Pos"]
+col_str = ", ".join(cols)
+items = ["MF"]
+conditions = ' OR '.join([f"Standard_Pos LIKE '%{item}%'" for item in items])
+query = f"""SELECT Standard_Player FROM `{project_id}.Stats.Players_2023` WHERE {conditions}"""
+test_df = pd.read_gbq(query, project_id=project_id)
+print(test_df.head())
