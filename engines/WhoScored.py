c596ffd5774cfe343ef45f7c5478083bf3d9facb
nreHieW <limweihern@gmail.com>
2024-06-23 12:51:57 +0800

    first success?

--- engines/WhoScored.py
+++ engines/WhoScored.py
@@ -10,12 +10,13 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from IPython.display import clear_output
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 
-from .request_utils import get_proxy, HEADERS, get_request
+# from .request_utils import get_proxy, HEADERS, get_request
 import json
 import os
 from tqdm import tqdm
+
+HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"}
 
 
 class WhoScored:
@@ -25,9 +26,9 @@ class WhoScored:
         options = Options()
         # # whoscored scraper CANNOT be headless
         # options.add_argument("window-size=700,600")
-        proxy = get_proxy()  # Use proxy
+        # proxy = get_proxy()  # Use proxy
+        proxy = {"http": "104.194.152.35:34567", "https": "104.194.152.35:34567"}
         print("Using proxy: {}".format(proxy))
-
         # options.add_argument(f"user-agent={HEADERS['user-agent']}")
         # options.add_argument("--ignore-certificate-errors")
         # options.add_argument("--headless")
@@ -37,21 +38,16 @@ class WhoScored:
         # self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)  # create driver
         # options.headless = True
         proxy = proxy["https"]
-        proxy_options = Proxy({"proxyType": ProxyType.MANUAL, "httpProxy": proxy, "sslProxy": proxy})
+        ip, port = proxy.split(":")
+        options.set_preference("network.proxy.type", 1)
+        options.set_preference("network.proxy.http", ip)
+        options.set_preference("network.proxy.http_port", int(port))
+        # options.set_preference("network.proxy.https", ip)
+        # options.set_preference("network.proxy.https_port", int(port))
 
-        # Set up Firefox options
-        firefox_options = Options()
-        firefox_options.proxy = proxy_options
-        # ip, port = proxy.split(":")
-        # options.set_preference("network.proxy.type", 1)
-        # options.set_preference("network.proxy.http", ip)
-        # options.set_preference("network.proxy.http_port", int(port))
-        # # options.set_preference("network.proxy.https", ip)
-        # # options.set_preference("network.proxy.https_port", int(port))
-
-        # options.set_preference("network.proxy.ssl", ip)
-        # options.set_preference("network.proxy.ssl_port", int(port))
-        # options.set_preference("general.useragent.override", HEADERS["user-agent"])
+        options.set_preference("network.proxy.ssl", ip)
+        options.set_preference("network.proxy.ssl_port", int(port))
+        options.set_preference("general.useragent.override", HEADERS["user-agent"])
 
         # self.driver = webdriver.Firefox(options=options, service=FirefoxService(executable_path="/usr/bin/geckodriver"))
         self.driver = webdriver.Firefox(options=options)
@@ -59,10 +55,10 @@ class WhoScored:
         print("=====================")
         print("Testing IP")
         print("=====================")
-        self.driver.get("http://httpbin.org/ip")
-        print(self.driver.find_element(By.TAG_NAME, "body").text)
+        # self.driver.get("http://httpbin.org/ip")
+        # print(self.driver.find_element(By.TAG_NAME, "body").text)
 
-        print("=====================")
+        # print("=====================")
         self.driver.get("https://deviceandbrowserinfo.com/info_device")
         print(self.driver.find_element(By.XPATH, "/html/body/main/section/div/p[4]").text)
         print("=====================")
@@ -127,7 +123,6 @@ class WhoScored:
         print("League page status: {}".format(self.driver.execute_script("return document.readyState")))
         print(self.driver.current_url)
         print(self.driver.title)
-        print("Elements", self.driver.find_elements(By.TAG_NAME, "select"))
         print(self.driver.find_element(By.TAG_NAME, "body").text)
 
         # Wait for season dropdown to be accessible, then find the link to the chosen season
