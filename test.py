a1a7a80c2dff2b5da844b3f3be0ec34b406b11f6
nreHieW <limweihern@gmail.com>
2025-07-25 01:08:26 -0400

    fix

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import undetected_chromedriver as uc
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(800, 800))
+display.start()
 
 driver = uc.Chrome(headless=False, use_subprocess=False)
 driver.get("https://whoscored.com/")
