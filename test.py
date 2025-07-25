23c5b117bcfbe519a66ed27f7b37c6df2eee2ad9
nreHieW <limweihern@gmail.com>
2025-07-25 01:11:24 -0400

    fix

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import undetected_chromedriver as uc
 from pyvirtualdisplay import Display
+from selenium_stealth import stealth
 
 display = Display(visible=0, size=(800, 800))
 display.start()
 
 driver = uc.Chrome(headless=False, use_subprocess=False)
+stealth(
+    driver,
+    languages=["en-US", "en"],
+    vendor="Google Inc.",
+    platform="Win32",
+    webgl_vendor="Intel Inc.",
+    renderer="Intel Iris OpenGL Engine",
+    fix_hairline=True,
+)
 driver.get("https://whoscored.com/")
 print(driver.page_source)
